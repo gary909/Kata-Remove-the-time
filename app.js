44374e62580527747a5c8f99d08886298302496e
Gary White <garywhite61@hotmail.com>
2022-12-21 12:59:58 +0100

    code

---
+++ app.js
@@ -0,0 +1,7 @@
+function shortenToDate(longDate) {
+    // your code here
+}
+
+console.log(shortenToDate("Friday May 2, 9am")); // "Friday May 2"
+console.log(shortenToDate("Tuesday January 29, 10pm")); // "Tuesday January 29"
+console.log(shortenToDate("Monday December 25, 10pm")); // "Monday December 25"
